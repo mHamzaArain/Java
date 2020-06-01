6e8d1e562f904df8cc24942ffa715658a3c54d25
mHamzaHanif <hamzaarain1999@gmail.com>
2020-06-02 02:50:19 +0500

    added

---
+++ learning/01_FirstProgram/FirstProgram.java
@@ -0,0 +1,20 @@
+//////////////// First Program by Saurabh Shukla
+// * Java use Camel Case.
+// * Java has 4 modifiers public, privatge, protected & default.
+// * Outer class must be public or protected.
+// * Class name must be as prgram name. i.e; Prgram.java -> class program
+
+public class FirstProgram {
+    // Public: It is public to call from outside
+    // static: Any static member can be called without making object.
+    // void: Does'nt return anything.
+    // String[] args: Array of string recieve args from CLI.
+    public static void main(String[] args) {
+        // System: Predefined class as it starts with capital letter.
+        // <class Name>. -> Dot after class name shows that static member(i.e; variable, function).
+        // out: Reference variable that represent object.
+        // out.println(): Call object member of reference variable "out"
+        System.out.println("Hello World");
+    }
+} // No semi colon as like C++
+
